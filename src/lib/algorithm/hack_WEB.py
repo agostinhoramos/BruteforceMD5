d1effea7ddf0ee0e722c4aa6976e680bf7502f6c
Tiago Cunha <1700597@sal.ipg.pt>
2020-05-15 19:48:52 +0100

    Estudo da pagina html

--- src/lib/algorithm/hack_WEB.py
+++ src/lib/algorithm/hack_WEB.py
@@ -11,3 +11,33 @@ class hack_WEB(App):
         self.main_url = self.obj['url_website']
         self.TOKEN = self.obj['app_token']
 
+    #STUDING HTML WEBPAGE
+    def HTMLformOBJ(self):
+        OBJ = {"url":[],"name":[],"method":[]}
+        html = urlopen(self.main_url)
+        soup = BeautifulSoup(html.read(), features="html.parser")
+        try:
+            values = soup.find_all('form')
+            for value in values:
+                v = value.get('action')
+                if v:
+                    OBJ["url"].append(v)
+        except:
+            return []
+        try:
+            values = soup.find_all("input")
+            for value in values:
+                v = value.get('name')
+                if v:
+                    OBJ["name"].append(v)
+        except:
+            return []
+        try:
+            values = soup.find_all('form')
+            for val in values:
+                v = val.get('method')
+                if v:
+                    OBJ["method"].append(v.upper())
+        except:
+            pass
+        return OBJ
