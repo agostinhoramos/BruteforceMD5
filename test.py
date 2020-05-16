1037ce42c31db4158d130540cee26b990e08314d
Agostinho Ramos <agostinhopina95@gmail.com>
2020-05-17 00:23:04 +0100

    update rainbowtable

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from src.lib.algorithm.app import App
 
 app = App()
-savepath = "src/data/attack/rainbowtable/Names.txt"
-with open("src/data/attack/dictionary/Names.dic", encoding="utf-8", errors="ignore") as fp:
+savepath = "src/data/attack/rainbowtable/faithwriters.txt"
+with open("src/data/attack/wordlist/faithwriters-withcount.txt", encoding="utf-8", errors="ignore") as fp:
     lines = ""
     line = fp.readline()
     while line :
-        word = line.strip()
-        lines = lines + app.md5(word)+';'+word+'\n'
+        word = line.strip().split(' ')[-1]
+        if len(word)>0:
+            lines = lines + app.md5(word)+';'+word+'\n'
         line = fp.readline()
-f = open(savepath, "w")
-f.write(lines)
-f.close()
+    f = open(savepath, "w")
+    f.write(lines)
+    f.close()
+    print('Done!')
